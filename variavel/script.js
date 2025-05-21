5af17ffc05672ef9b9829e72dfa41546f3e590a7
BRYAN FERREIRA DOS REIS <bryan.freis@senacsp.edu.br>
2025-05-21 16:42:58 -0300

    update

---
+++ variavel/script.js
@@ -0,0 +1,24 @@
+//comentário em uma linha
+
+/* comentário 
+em várias 
+linhas */
+
+// Exibir comando na tela
+console.log(2);
+
+console.log(typeof 2.5);
+
+// Declarando constantes
+const meunumero = 10;
+console.log(meunumero);
+
+// Constante não pode ser mudada - Type error
+/* meunumero = 15;
+console.log(meunumero); */
+
+// Declarando variáveis
+let novonumero = 100;
+novonumero = 200;
+console.log(novonumero)
+
